23a5b220f6cce2d18a5d75d09a540a2683cad3b2
SeyedKia <seyedkia27@gmail.com>
2016-05-27 13:01:02 +0430

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -3,9 +3,9 @@ local function callbackres(extra, success, result) -- Callback for res_user in l
   local user = 'user#id'..result.id
 	local chat = 'chat#id'..extra.chatid
 	if is_banned(result.id, extra.chatid) then -- Ignore bans
-            send_large_msg(chat, '')
+            send_large_msg(chat, 'User is banned.')
 	elseif is_gbanned(result.id) then -- Ignore globall bans
-	    send_large_msg(chat, '')
+	    send_large_msg(chat, 'User is globaly banned.')
 	else    
 	    chat_add_user(chat, user, ok_cb, false) -- Add user on chat
 	end
@@ -23,9 +23,9 @@ function run(msg, matches)
   if not is_momod(msg) then
     return
   end
-  if not is_admin(msg) then -- For admins only !
-    return ''
-  end
+  --if not is_admin(msg) then -- For admins only !
+    --return 'Only admins can invite.'
+  --end
 	local cbres_extra = {chatid = msg.to.id}
   local username = matches[1]
   local username = username:gsub("@","")
@@ -33,7 +33,7 @@ function run(msg, matches)
 end
 return {
     patterns = {
-      "^([Ii]nv) (.*)$"
+      "^[!/]invite (.*)$"
     },
     run = run
 }
