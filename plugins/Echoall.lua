54de43d250445b818bc30239306f0cc058fa5d3d
Seyed Ali Kia <seyedkia27@gmail.com>
2016-07-06 10:59:10 +0430

    #Last Edit

--- plugins/Echoall.lua
+++ plugins/Echoall.lua
@@ -1,14 +1,26 @@
+
 local function run(msg, matches)
   local text = matches[1]
+  local b = 1
+
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','') -- Delete ! from first of pm!
+    text,b = text:gsub('^/+','') -- Delete / from first of pm!
+    text,b = text:gsub('^#+','') -- Delete # from first of pm!
+  end
+  if is_sudo(msg) then
   return text
 end
-
+end
 return {
-  description = "Echo every message",
-  usage = "Just active it",
+  description = "Echo Every Message !",
+  usage = "Return Every Message !",
   patterns = {
-    "^(.+)$"
+    "^(.+)$",
   }, 
   run = run 
 }
--- Active It With Cation !
+
+-- Plugin By @Pokr_Face
+-- Edit By @NuLLUser :)
