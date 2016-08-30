403c597b51a0185da8c2eca7d4b2001b218efc1f
/Seyed <seyedkia27@gmail.com>
2016-08-30 12:21:54 +0430

    Leave

--- plugins/Leave.lua
+++ plugins/Leave.lua
@@ -1,26 +1,19 @@
 do
 -- When sudo sends leave, Xy will leave group
-local function run(msg, matches)
+local function runs(msg, matches)
 local bot_id = our_id 
-local receiver = get_receiver(msg)
-    if matches[1]:lower() == 'leave' and is_sudo(msg) then
+    if matches[1]:lower() == "leave" and is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-	   --leave_channel(receiver, ok_cb, false)
-           --elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
-           --send_large_msg(receiver, 'This is not one of my groups.', ok_cb, false)
-           --chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
-	   --leave_channel(receiver, ok_cb, false)
     end
 end
  
 return {
   patterns = {
     "^([Ll]eave)$",
-    --"^!!tgservice (.+)$",
   },
-  run = run
+  run = runs
 }
 end
 
--- Plugin By SeedTeam
--- Edit By @NuLLUser :)
+-- Main Plugin >> https://github.com/SEEDTEAM/TeleSeed/blob/master/plugins/onservice.lua
+-- Edited By @NuLLuseR :)
