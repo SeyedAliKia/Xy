50cbb6f84c7159295afef09a090f0211c78d89ea
/Seyed <seyedkia27@gmail.com>
2016-09-09 12:53:59 +0430

    Update Adder.lua

--- plugins/Adder.lua
+++ plugins/Adder.lua
@@ -1,16 +1,1 @@
-do
-local function runs(msg, matches)
-local bot_id = 114760915 
-    if matches[1]:lower() == "leave" and is_sudo(msg) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-       return "fuck"
-    end
-end
- 
-return {
-  patterns = {
-    "^([Ll]eave)$",
-  },
-  run = runs
-}
-end
+,
