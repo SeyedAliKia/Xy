f031bf3f3c29c50706612eac3abfa9e380b57d6d
/Seyed <seyedkia27@gmail.com>
2016-09-09 12:42:31 +0430

    Update Adder.lua

--- plugins/Adder.lua
+++ plugins/Adder.lua
@@ -1,40 +1,21 @@
+ocal function add_user(chat, user)
+   local status = chat_add_user (chat, user, ok_cb, false)
+   if not status then
+   end
+end
+    local function runs(msg, matches)
+           if matches[1] == 'chat_del_user' then
+                local user = 'chat#id'..msg.to.id 
+                local chat = 'chat#id'..msg.from.id
+                --add_user("chat#id"..msg.to.id, "user#id"..msg.action.user.id)
+                return " "..user.." \n ""..chat..
+                --return "" -- You can put everything here for returing when someone is added !
+    end
+end
 
-do
-local function callback(extra, success, result)
-vardump(success)
-vardump(result)
-end
-local function run(msg, matches)
-local user = 203213812
-if matches[1] == "add" then
-user = 'user#id'..user
-end
-if is_owner(msg) then
-    if msg.from.username ~= nil then
-      if string.find(msg.from.username , 'ParsTG') then
-          return "Added"
-          end
-if msg.to.type == 'chat' then
-local chat = 'chat#id'..msg.to.id
-chat_add_user(chat, user, callback, false)
-return "Adding..."
-end
-elseif not is_owner(msg) then
---return 'شما دسترسی برای دعوت صاحب ربات را ندارید'
-end
-end
-end
 return {
-description = "insudo",
-usage = {
-"!invite name [user_name]",
-"!invite id [user_id]" },
-patterns = {
-"^[!/](add)$",
-"^([Aa]dd)$"
-
-},
-run = run
+    patterns = {
+        "^!!tgservice (chat_del_user)$",
+        },
+    run = runs
 }
-end
-
