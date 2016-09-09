b36bdf0dcf73f7119e84b1a48135b81c6a27c309
/Seyed <seyedkia27@gmail.com>
2016-09-09 12:52:17 +0430

    Update Adder.lua

--- plugins/Adder.lua
+++ plugins/Adder.lua
@@ -1,9 +1,8 @@
 do
--- When sudo sends leave, Xy will leave group
 local function runs(msg, matches)
-local bot_id = our_id 
+local bot_id = 114760915 
     if matches[1]:lower() == "leave" and is_sudo(msg) then
-       --chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
        return "fuck"
     end
 end
