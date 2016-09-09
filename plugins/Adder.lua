f8a2f77b16699df715b6b17b7d515fe05677ab6e
/Seyed <seyedkia27@gmail.com>
2016-09-09 12:46:04 +0430

    Update Adder.lua

--- plugins/Adder.lua
+++ plugins/Adder.lua
@@ -1,8 +1,3 @@
-local function add_user(chat, user)
-   local status = chat_add_user (chat, user, ok_cb, false)
-   if not status then
-   end
-end
     local function runs(msg, matches)
            if matches[1] == 'add' then
                 local user = 'chat#id'..msg.to.id 
@@ -14,7 +9,7 @@ end
 
 return {
     patterns = {
-    '^[Ss]erverinfo$'
+    '^[Aa]dd$'
         },
     run = runs
 }
