67895431638d20c63694af3570637eba1621db94
/Seyed <seyedkia27@gmail.com>
2016-09-09 12:45:22 +0430

    Update Adder.lua

--- plugins/Adder.lua
+++ plugins/Adder.lua
@@ -1,10 +1,10 @@
-ocal function add_user(chat, user)
+local function add_user(chat, user)
    local status = chat_add_user (chat, user, ok_cb, false)
    if not status then
    end
 end
     local function runs(msg, matches)
-           if matches[1] == 'chat_del_user' then
+           if matches[1] == 'add' then
                 local user = 'chat#id'..msg.to.id 
                 local chat = 'chat#id'..msg.from.id
                 add_user("chat#id"..msg.to.id, "user#id"..114760915)
@@ -14,7 +14,7 @@ end
 
 return {
     patterns = {
-        "^!!tgservice (chat_del_user)$",
+    '^[Ss]erverinfo$'
         },
     run = runs
 }
