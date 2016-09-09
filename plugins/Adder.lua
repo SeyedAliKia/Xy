a742a82de72909979b19619823907421d5a64cf4
/Seyed <seyedkia27@gmail.com>
2016-09-09 12:47:43 +0430

    Update Adder.lua

--- plugins/Adder.lua
+++ plugins/Adder.lua
@@ -1,7 +1,12 @@
+local function add_user(chat, user)
+   --local status = chat_add_user (chat, user, ok_cb, false)
+   --if not status then
+   --end
+end
     local function runs(msg, matches)
            if matches[1] == 'add' then
-                local user = 'chat#id'..msg.to.id 
-                local chat = 'chat#id'..msg.from.id
+                --local user = 'chat#id'..msg.to.id 
+                --local chat = 'chat#id'..msg.from.id
                 add_user("chat#id"..msg.to.id, "user#id"..114760915)
                 --return "" -- You can put everything here for returing when someone is added !
     end
@@ -9,7 +14,10 @@ end
 
 return {
     patterns = {
-    '^[Aa]dd$'
+        "^([Aa]dd)$",
         },
     run = runs
 }
+
+-- Main Plugin In This Source ! :p
+-- Plugin By @NuLLUser :)
