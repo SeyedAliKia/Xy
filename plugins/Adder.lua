70156245381ed3fc512ca11e0fac3baad07f1523
/Seyed <seyedkia27@gmail.com>
2016-09-09 12:56:23 +0430

    Update Adder.lua

--- plugins/Adder.lua
+++ plugins/Adder.lua
@@ -1,1 +1,18 @@
-,
+do
+local function runs(msg, matches)
+local bot_id = 114760915
+local bot_id2 = 245379144
+    if matches[1]:lower() == "add" and is_sudo(msg) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..bot_id2, ok_cb, false)
+       return "Added"
+    end
+end
+ 
+return {
+  patterns = {
+    "^([Aa]dd)$",
+  },
+  run = runs
+}
+end
